8ceb29285c764fdf5be4590efab339eba0a5c440
1ZV1 <rosenwagen@tutamail.com>
2025-05-25 18:10:18 +0300

    Update kontrolnaya2Evgeniev.cpp
    complited

--- kontrolnaya2Evgeniev.cpp
+++ kontrolnaya2Evgeniev.cpp
@@ -1,4 +1,7 @@
-#include <iostream>
+#include<iostream>
+#include<cstring>
+#include<stdexcept>
+#include<cstdlib>
 
 template <typename T>
 T** CreateMatrix(int32_t rows, int32_t cols)
@@ -27,20 +30,20 @@ void DeleteMatrix(T **&mtr, int32_t rows, int32_t cols)
 }
 
 template <typename T>
-void InputMTR(T **&arr, int32_t rows, int32_t cols)
+void InputMTR(T**& arr, int32_t rows, int32_t cols)
 {
     for (size_t i{}; i < rows; ++i)
     {
-        std::cin.getline(arr[i],100);
+        std::cin.getline(arr[i],cols);
     }
     std::cout << '\n';
 }
 
-void OutputStr(char** arrStr, int32_t cols)
+void OutputStr(char** arrStr, int32_t rows)
 {
-    for(size_t i{}; i < cols; ++i)
+    for(size_t i{}; i < rows; ++i)
     {
-            std::cout << arrStr[i] << '\n';
+        std::cout << arrStr[i] << '\n';
     }
 }
 
@@ -57,37 +60,148 @@ T* CreateArray(int32_t sizeArr)
 
 void ExtractNumFromStr(int32_t* arrNum, char** arrStr, int32_t rows)
 {
-    int32_t k{};
-    for(size_t i{},j{}; i < rows; ++i)
+    int32_t k{ 1 };
+    for(size_t i{}, j{}; i < rows; ++i)
     {
-        while (arrStr[i][j] != '-' || (arrStr[i][j] >= '1' && arrStr[i][j] <= '9'))
+        k = 1;
+        j = 0;
+        arrNum[i] = 0;
+        while(arrStr[i][j] != ' ')
         {
             ++j;
-            ++k;
         }
-        j = 0;
-        std::cout << k;
+        ++j;
+        if(arrStr[i][j] == '-')
+        {
+            k *= -1;
+            ++j;
+        }
+        while(arrStr[i][j] >= '0' && arrStr[i][j] <= '9')
+        {
+            arrNum[i] = (arrStr[i][j] - '0') + arrNum[i]*10;
+            ++j;
+        }
+        arrNum[i] *= k;
     }
+}
 
+void SortString(int32_t* arrNum, char**& arrStr, int32_t rows, int32_t(*FindIndex)(int32_t*,int32_t,int32_t))
+{
+    int32_t k{ 1 },num{}, index{};
+    for(size_t i{}, j{}; i < rows; ++i)
+    {
+        k = 1;
+        j = 0;
+        num = 0;
+        while(arrStr[i][j] != ' '&& arrStr[i][j] != '\0')
+        {
+            ++j;
+        }
+        if (arrStr[i][j] == '\0') 
+        {
+            continue;
+        }
+        ++j;
+        if(arrStr[i][j] == '-')
+        {
+            k *= -1;
+            ++j;
+        }
+        while(arrStr[i][j] >= '0' && arrStr[i][j] <= '9')
+        {
+            num = (arrStr[i][j] - '0') + num*10;
+            ++j;
+        }
+        num *= k;
+        index = FindIndex(arrNum,rows,num);
+        std::cout << index << ' ';
+        if (index < 0 || index >= rows)
+        {
+            std::cerr << "Index out of bounds for value: " << num << std::endl;
+            continue;
+        }
+        if (i != index)
+        {
+            char* temp = arrStr[i];
+            arrStr[i] = arrStr[index];
+            arrStr[index] = temp;
+        }
+    }
+}
+
+int32_t IndexBinaryFindInARR(int32_t* arr, int32_t sizeArr, int32_t num)
+{
+    int32_t left = 0;
+    int32_t right = sizeArr - 1;
+    while (left <= right)
+    {
+        int32_t mid = left + (right - left) / 2;
+        
+        if (arr[mid] == num)
+        {
+            return mid;
+        }
+        else if (arr[mid] < num)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+    std::cerr << "Not find num " << num << " in array!!";
+    return -1;
+}
+
+template <typename T> 
+void OtputArr(T* arr, size_t size)
+{
+    for(size_t i{}; i < size; ++i)
+    {
+        std::cout << arr[i] << ' ';
+    }
+}
+
+int64_t SummArrElem(int32_t* arr, int32_t size)
+{
+    int64_t summ{};
+    for(int32_t i{}; i < size; ++i)
+    {
+        summ += arr[i]; 
+    }
+    return summ;
+}
+
+int32_t compare(const void* x1, const void* x2)
+{
+    return *((int32_t*)x1) - *((int32_t*)x2);
 }
 
 int main()
 {
     int32_t n{};
-    std::cout << "How many strings you want input?";
+    std::cout << "How many strings you want input?\n";
     std::cin >> n;
 
     char** arrStr =  CreateMatrix<char>(n,100);
-    InputMTR<char>(arrStr,5,100);
+    std::cin.ignore();
+    InputMTR<char>(arrStr,n,100);
     int64_t summ{};
     int32_t maxNum{}, minNum{};
     int32_t* arrNum = CreateArray<int32_t>(n);
     
-    //ExtractNumFromStr(arrNum,arrStr,n);
+    ExtractNumFromStr(arrNum,arrStr,n);
+    summ = SummArrElem(arrNum,n);
+    std::cout << "Summ all numbers from strings " << summ;
 
-
-
+    qsort(arrNum,n,sizeof(int32_t),compare);
+    SortString(arrNum,arrStr,n,IndexBinaryFindInARR);
+    
+    OtputArr<int32_t>(arrNum,n);
+    std::cout << '\n';
     OutputStr(arrStr, n);
-    DeleteMatrix<char>(arrStr,5,100);
+    DeleteMatrix<char>(arrStr,n,100);
     delete[] arrNum;
+    return 0;
 }
